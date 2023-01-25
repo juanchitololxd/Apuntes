4f9f22d580a4e6934cfca52dc3956fe608224e0e
Juan Fonseca <juanpablofonsecac5@gmail.com>
2023-01-25 17:02:34 -0500

    apuntes para 2023-1

--- RECO/OTROS/Labs/shells/procesosW.sh
+++
@@ -1,9 +0,0 @@
-no=1
-number=10
-echo debug1
-while [ $no -le $number ]
-do
-echo debug2
-no=$((no+1))
-done
-echo debug3
--- RECO/OTROS/Labs/shells/windows/archivos_grandes.ps1
+++
@@ -1,3 +0,0 @@
-#| sort -descending -property length | select -first $args[0] name, length
-echo "a"
-gci -r
